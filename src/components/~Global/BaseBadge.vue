370006a586ba3d7fbd93fb38239b27f45c143d5a
Dejkuu <dawio246@op.pl>
2024-02-11 21:51:42 +0100

    new component

---
+++ src/components/~Global/BaseBadge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="base__badge flex-center q-py-xs q-px-sm bg-infoContainer text-onInfoContainer border-info rounded-borders gap-xs box-shadow"
+  >
+    <q-icon :name="icon" class="base__badge__icon text-size-8" />
+
+    <div class="base__badge__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: undefined,
+  },
+});
+</script>
