54769e177ec197374500f5e3663fc416bcb38b09
Dejkuu <dawio246@op.pl>
2024-03-03 23:42:27 +0100

    another transitions rework

--- src/boot/routesTransitions.ts
+++ src/boot/routesTransitions.ts
@@ -7,24 +7,30 @@ export default boot(({ router }) => {
       const to_PositionOnNavBar = to.meta.positionOnNavBar as number;
       const from_PositionOnNavBar = from.meta.positionOnNavBar as number;
 
-      to.meta.transition = to_PositionOnNavBar < from_PositionOnNavBar ? 'slide-right' : 'slide-left';
+      to.meta.transition = to_PositionOnNavBar < from_PositionOnNavBar ? 'page-slide-right' : 'page-slide-left';
+
+      return true;
     }
 
     // from special page to login or signup page
     if ((from.path == '/' || from.path == '/loggedOut' || from.path == '/accountDeleted') && (to.path == '/login' || to.path == '/signup')) {
-      to.meta.transition = 'slide-down'
+      to.meta.transition = 'page-slide-down';
+
+      return true;
     }
 
     // from another page to nav
     if (from.meta.positionOnNavBar == undefined && to.meta.transition == undefined) {
-      to.meta.transition = from.meta.transition == 'slide-right' ? 'slide-left' : 'slide-right'
+      to.meta.transition = from.meta.transition == 'page-slide-right' ? 'page-slide-left' : 'page-slide-right';
+
+      return true;
     };
 
     // from support pages
     if (from.name == 'Support' && to.name == 'Support Center') {
-      to.meta.transition = 'slide-right'
-    };
+      to.meta.transition = 'page-slide-right';
 
-    return true;
+      return true;
+    };
   })
 })
