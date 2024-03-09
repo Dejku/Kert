a5bbc9b919d76b1151ccb4483d353b7a9d66ac55
Dejkuu <dawio246@op.pl>
2024-03-09 16:14:08 +0100

    added new snapshot

--- src/helpers/database/snapshots.ts
+++ src/helpers/database/snapshots.ts
@@ -2,10 +2,15 @@ import { useAccountStore } from 'stores/accountStore';
 import { useVacationStore } from 'stores/vacationStore';
 import { useNewsStore } from 'stores/newsStore';
 import { usePreferenceStore } from 'stores/preferenceStore';
+import { useTaskStore } from 'stores/taskStore';
 
 import { Unsubscribe, doc, getFirestore, onSnapshot } from 'firebase/firestore';
 
-let vacationStoreUnsubscribe: Unsubscribe, newsStoreUnsubscribe: Unsubscribe, preferenceStoreUnsubscribe: Unsubscribe;
+let vacationStoreUnsubscribe: Unsubscribe,
+    newsStoreUnsubscribe: Unsubscribe,
+    preferenceStoreUnsubscribe: Unsubscribe,
+    taskStoreUnsubscribe: Unsubscribe;
+
 export function snapshotsStart() {
     const db = getFirestore();
 
@@ -13,6 +18,7 @@ export function snapshotsStart() {
     const vacationStore = useVacationStore();
     const newsStore = useNewsStore();
     const preferenceStore = usePreferenceStore()
+    const taskStore = useTaskStore()
 
     vacationStoreUnsubscribe = onSnapshot(doc(db, 'vacationStore', accountStore.getID), (doc) => {
         vacationStore.overdueVacationDays = doc.data()?.overdueVacationDays;
@@ -24,10 +30,13 @@ export function snapshotsStart() {
     newsStoreUnsubscribe = onSnapshot(doc(db, 'newsStore', accountStore.getID), (doc) => newsStore.news = doc.data()?.news);
 
     preferenceStoreUnsubscribe = onSnapshot(doc(db, 'preferenceStore', accountStore.getID), (doc) => preferenceStore.preference = doc.data()?.preference);
+
+    taskStoreUnsubscribe = onSnapshot(doc(db, 'taskStore', accountStore.getID), (doc) => taskStore.sets = doc.data()?.sets);
 }
 
 export function snapshotsStop() {
     vacationStoreUnsubscribe();
     newsStoreUnsubscribe();
     preferenceStoreUnsubscribe();
+    taskStoreUnsubscribe();
 }
